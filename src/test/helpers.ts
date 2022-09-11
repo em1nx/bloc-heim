4416afb386cb976c3fa9a22a521c22b8cff4545a
Emin Chernov <mail@emin.pro>
2022-09-11 15:28:49 +0300

    wrap with bloc provider test

---
+++ src/test/helpers.ts
@@ -0,0 +1,16 @@
+import { Position, Range, TextDocument, window, workspace } from "vscode";
+
+export async function openFile(content: string, position: Position = new Position(0,0)): Promise<TextDocument> {
+    const document = await createDocument(content, 'dart');
+    await window.showTextDocument(document, {
+        selection: new Range(position.line, position.character, position.line, position.character)
+    });
+    return document;
+}
+
+async function createDocument(content: string, language?: string) {
+    return await workspace.openTextDocument({
+        language,
+        content,
+    });
+}
