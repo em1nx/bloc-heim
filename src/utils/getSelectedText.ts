fc765b6a3fd0db812a91a250e5f3d485ed8c3f9e
Emin Chernov <mail@emin.pro>
2022-09-26 12:55:48 +0600

    const prefix support added (#24)

--- src/utils/getSelectedText.ts
+++ src/utils/getSelectedText.ts
@@ -1,6 +1,7 @@
 
-// Forked from https://github.com/felangel/bloc/blob/313e963d9374871395a6099f753975498c037267/extensions/vscode/src/utils/get-selected-text.ts
+/// Forked from https://github.com/felangel/bloc/blob/313e963d9374871395a6099f753975498c037267/extensions/vscode/src/utils/get-selected-text.ts
 
+import { setEngine } from "crypto";
 import { TextEditor, Selection, Position } from "vscode";
 
 const openBracket = "(";
@@ -46,9 +47,12 @@ export const getSelectedText = (editor: TextEditor): Selection => {
         ? bracketIndex
         : lineText.length;
 
-    return new Selection(
-      new Position(line.lineNumber, widgetStartIndex),
-      new Position(line.lineNumber, endIndex)
+    return considerConstKeyword(
+      lineText, 
+      new Selection(
+        new Position(line.lineNumber, widgetStartIndex),
+        new Position(line.lineNumber, endIndex)
+      ),
     );
   }
 
@@ -61,13 +65,34 @@ export const getSelectedText = (editor: TextEditor): Selection => {
       if (currentChar === openBracket) {bracketCount++;}
       if (currentChar === closeBracket) {bracketCount--;}
       if (bracketCount === 0) {
-        return new Selection(
-          new Position(line.lineNumber, widgetStartIndex),
-          new Position(l, c + 1)
-        );
+        return considerConstKeyword(
+          lineText,
+          new Selection(
+            new Position(line.lineNumber, widgetStartIndex),
+            new Position(l, c + 1)
+          ),
+        ); 
       }
     }
   }
 
   return emptySelection;
 };
+
+const considerConstKeyword = (lineText: String, selection: Selection) => {
+  const constOffset = 6;
+  const checkConst = lineText.substring(
+    selection.anchor.character - constOffset, 
+    selection.anchor.character - constOffset + 5,
+  );
+  if (checkConst === 'const') {
+    return new Selection(
+      new Position(selection.anchor.line, selection.anchor.character - constOffset), 
+      selection.active,
+    );
+  }
+  else {
+    return selection;
+  };
+};
+
