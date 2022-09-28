cc79f6612a421029ef3ff976ef466580f167641d
Emin Chernov <mail@emin.pro>
2022-09-28 22:52:42 +0600

    Don't show code actions for empty selection (#37)

--- src/code-actions/blocCodeActionsProvider.ts
+++ src/code-actions/blocCodeActionsProvider.ts
@@ -1,7 +1,18 @@
-import { CodeAction, CodeActionKind, CodeActionProvider } from "vscode";
+import { CodeAction, CodeActionKind, CodeActionProvider, window } from "vscode";
+import { getSelectedText } from "../utils";
 
 export class BlocCodeActionProvider implements CodeActionProvider {
   public provideCodeActions(): CodeAction[] {
+    const editor = window.activeTextEditor;
+    if (!editor) {
+      return [];
+    }
+
+    const selectedText = editor.document.getText(getSelectedText(editor));
+    if (selectedText === "") {
+      return [];
+    }
+
     return [
       {
         command: "bloc-heim.wrapWithBlocProvider",
