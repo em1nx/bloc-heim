e12adf75023147e1e1ca0c792b01b431e2a9579d
Emin Chernov <mail@emin.pro>
2022-10-02 20:49:38 +0600

    Add addBlocStateProperty command to code actions

--- src/utils/bloc-state-parser/index.ts
+++ src/utils/bloc-state-parser/index.ts
@@ -1,5 +1,4 @@
 
-export * from './parseBlocStateClass';
 export * from './blocStateClass';
 export * from './getClassReflection';
 export * from './isBlocStateClass';
--- src/utils/bloc-state-parser/parseBlocStateClass.ts
+++
@@ -1,6 +0,0 @@
-
-export function parseBlocStateClass(text: string) {
-    // найти код класса от курсора
-    // орентируясь на поле status расставить плейсхолдеры
-    // добавить свойство через SnippetString   
-}
