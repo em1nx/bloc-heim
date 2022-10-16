1ecea1861a3c9747fb29fa4fab2e724aed01c417
Emin Chernov <mail@emin.pro>
2022-10-16 12:52:59 +0200

    Fix add bloc state property with multiline props (#42)

--- src/utils/bloc-state-parser/isBlocStateClass.ts
+++ src/utils/bloc-state-parser/isBlocStateClass.ts
@@ -4,7 +4,7 @@ import { ClassReflection, getClassReflection } from "./getClassReflection";
 const blocStateClassPattern = (className: string) => new RegExp(`^class\\s+${className}\\s+extends\\s+Equatable`);
 const stateStatusFieldPattern = (blocName: string) => new RegExp(`\\s+final ${blocName}Status status;`);
 const stateStatusConstructorPattern = () => /required\s+this\.status,/;
-const stateStatusPropsPattern = () => /List<Object\?> get props => \[.*[^\w]?status[^\w]?.*\];/;
+const stateStatusPropsPattern = () => /List<Object\?> get props => \[.*[^\w]?status[^\w]?.*\];/s;
 
 export function isBlocStateClass(classRefelection: ClassReflection): boolean {
     const patterns = [
